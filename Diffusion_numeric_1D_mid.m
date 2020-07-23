b8b91f269fd86dcef9ba8b2141653fa89011e442
M-Bergin <mbergin1992@hotmail.com>
2020-07-23 16:44:40 +1000

    Updated Diffusion_numeric_1D_mid comments

--- Diffusion_numeric_1D_mid.m
+++ Diffusion_numeric_1D_mid.m
@@ -4,29 +4,6 @@ function [u_out,v_out,x]=Diffusion_numeric_1D_mid(x_in,D_gen,flip,delta_t,L,N_x,
 %Based on http://www.atmos.albany.edu/facstaff/brose/classes/ATM623_Spring2015/Notes/Lectures/Lecture16%20--%20Numerical%20methods%20for%20diffusion%20models.html
 
 
-%Old setup commented out and instead fed in as parameters to the function
-%D_gen= @(u_D) a+c*exp(-(u_D-u0).^2/(2*b^2));       %Gaussian
-%D_gen= @(u_D) (u_D>u1 & u_D<u2)*c +a;              %Step layer
-%D_gen= @(u_D) (u_D>u1 & u_D<u2)*c +a+(u_D>=u2)*c_sig;  %Three layer
-%D_gen= @(u_D) (u_D>u0)*a +c;                        %Single step
-%D_gen= @(u_D) c_sig./(1+exp(-(u_D-u0)/b_sig))+a;           %Sigmoid
-%D_gen= @(u_D) c_sig./(1+exp(-(u_D-u0)/b_sig))+a+c*exp(-(u_D-u0).^2/(2*b^2));
-% N_x=301;
-% L=150;
-% delta_t=5e-3;
-% T=300;
-% %N_t=11;
-% %D=0.01;
-% u_test=[0:1e-3:1];
-% %a0=0.96;
-% %b0=0.05;
-% %c0=0.65;
-% %u0=0.46;
-% D_test=D_gen(u_test);
-
-%N_plot=50;
-
-
 %Initialise variables
 N_t=floor(T/(delta_t)+1);
 delta_x=L/(N_x);
@@ -45,7 +22,7 @@ x=x_stag(1:end-1)+delta_x/2;
 % figure;plot(u_test,D_test)
 
 
-%Initial conditions
+%Initial conditions, can flip to either have the left or right sides high
 u=zeros(size(x));
 if flip==1
     u(floor(N_x/2)+1:end)=1;
@@ -77,19 +54,11 @@ for n=1:N_t
     dudx=diff(u)./delta_x;
     
     %No flux at the ends boundary condition
-    %dudx(1)=0;
-    %dudx(N_x)=0;
     dudx=[0,dudx,0];
     
     %Calculate diffusivity at each point    
     u_D=[u,u(end)];
     u_D2=[u(1),u];
-    %a0=0.96;
-    %b0=0.05;
-    %c0=0.65;
-    %x0=0.46;
-    %D=a+c*b./((u_D-u0).^2+b^2);
-    %D=a+c*normpdf(u_D,u0,b);
     D=D_gen((u_D+u_D2)/2); %Nonsense at the boundaries? but works elsewhere
     
     %Calculate flux (kind of)
